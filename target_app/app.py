abace6550eb3d44a66ef91158640b8ae2d2e1e04
Aashi <aashi.possible@gmail.com>
2026-08-28 17:21:36 +0530

    Fix target app entry point

--- target_app/app.py
+++ target_app/app.py
@@ -1,10 +1,8 @@
-from .bug import buggy_function
+from .bug import run_bug
 
 
 def run(condition):
     """
     Entry point for ALMOST to test the target application.
     """
-
-    buggy_function(condition)
-    
+    return run_bug(condition)
